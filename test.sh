c312ff24bac04e23fc082dc417cf01b7abe11c0f
mark <mark@havak.nl>
2018-01-31 15:59:50 +0100

    Further Housekeeping

--- test.sh
+++
@@ -1,1 +0,0 @@
-[ -b /dev/loop0 ] && losetup -d /dev/loop0 || echo Loop Device Free
--- umount.sh
+++
@@ -1,5 +0,0 @@
-umount /tmp/temp/home
-umount /tmp/temp/var
-umount /tmp/temp/boot
-umount /tmp/temp/proc
-umount /tmp/temp/
